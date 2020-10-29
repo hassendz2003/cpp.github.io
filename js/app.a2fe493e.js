9c25619c8c8410c2e36427e637cac73ab3f7aacf
Jesus <bcmail@gmail.com>
2020-10-29 12:57:16 -0500

    mainnet1

--- js/app.a2fe493e.js
+++ js/app.a2fe493e.js
@@ -426,7 +426,7 @@
                     }
                 }, [t._v("Referral")])], 1), n("li", [n("a", {
                     attrs: {
-                        href: "https://shasta.tronscan.org/#/contract/TZ5p2Z41VHSVBSxZWvXhdzMgxExXc1G8Yx"
+                        href: "https://tronscan.org/#/contract/TAs9RZmVgwQWPeZEPdH4xq5EWtpA4DumpT"
                     }
                 }, [t._v("Contract")])], 1), n("li", [n("a", {
                     attrs: {
@@ -542,7 +542,7 @@
                     }
                 }, [t._v("Referral")])], 1), n("li", [n("a", {
                     attrs: {
-                        href: "https://shasta.tronscan.org/#/contract/TZ5p2Z41VHSVBSxZWvXhdzMgxExXc1G8Yx"
+                        href: "https://tronscan.org/#/contract/TAs9RZmVgwQWPeZEPdH4xq5EWtpA4DumpT"
                     }
                 }, [t._v("Contract")])], 1), n("li", [n("a", {
                     attrs: {
@@ -555,7 +555,7 @@
             i = a.n(o),
             l = a("7f45"),
             c = a.n(l),
-            u = "TZ5p2Z41VHSVBSxZWvXhdzMgxExXc1G8Yx",
+            u = "TAs9RZmVgwQWPeZEPdH4xq5EWtpA4DumpT",
             d = localStorage.getItem("referrerAddress") || "TUkY7QsBEX4EBSvhuTFeswFTap8UV3baYc";
 
         function f(t, e, a, n, r, s, o) {
